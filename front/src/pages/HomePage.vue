c53006f43e7a431f9ddac13a9adbb1cade735580
neycha <regentmarysapro@gmail.com>
2025-04-11 09:59:50 +0200

    fix: delete test router link to /users

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
     <h2 class="test-title">Bienvenue sur CodeSwap</h2>
-    <router-link to="/test">Aller à la page Test</router-link>
+    <router-link to="/users">Aller à la page des utilisateurs</router-link>
   </div>
 </template>
+
+<script setup lang="ts"></script>
