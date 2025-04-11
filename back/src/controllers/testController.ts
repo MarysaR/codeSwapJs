d352917866d4a4086fbadbca6ac5a4cf5ee200b4
neycha <regentmarysapro@gmail.com>
2025-04-11 08:54:24 +0200

    feat: delete Test files

--- back/src/controllers/testController.ts
+++
@@ -1,9 +0,0 @@
-import { TestUseCase } from "logic";
-import { Request, Response } from "express";
-
-export const testLogic = (req: Request, res: Response) => {
-  console.log("testLogic a été appelé !");
-  const result = TestUseCase.run();
-
-  res.send({ message: result });
-};
--- logic/src/useCases/testUseCase.ts
+++
@@ -1,6 +0,0 @@
-export class TestUseCase {
-  static run() {
-    console.log("TestUseCase exécuté !");
-    return "Communication entre `front` et `logic` OK !";
-  }
-}
